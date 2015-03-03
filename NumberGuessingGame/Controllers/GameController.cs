d852293b63e749a05ad6d25a596198baebc3bd4b
sh222td <sh222td@student.lnu.se>
2015-03-03 14:37:53 +0100

    gissadethemligatalet

---
+++ NumberGuessingGame/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using NumberGuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NumberGuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            var model = new GuessingGame();
+            return View();
+        }
+    }
+}
---
+++ NumberGuessingGame/Models/GuessingGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NumberGuessingGame.Models
+{
+    public class GuessingGame
+    {
+        public int Number { get; set; }
+    }
+}
